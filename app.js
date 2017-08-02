8a29903cf52555f484f08ad0e0771f2f578c46f0
Jennifer Elton <justjenny40@hotmail.com>
2017-08-02 12:24:22 -0700

    .\.eslintrc

---
+++ app.js
@@ -0,0 +1,33 @@
+ /* <script>
+  alert('Hello');
+     var years=prompt('How many years have you been coding?');
+     console.log( 'They have been coding: ' +  years);
+
+    prompt( "You have been coding for " + years + "?");
+    var current= prompt( 'what are you currently learning?');
+
+    alert("Lets hope I get as good at " + current + " as you are! ");
+    console.log( 'They are currently learning ' + current);
+</script> */
+
+
+
+
+
+
+  <script>
+     alert prompt("Welcome to my page, lets see what you know about me!");
+     
+     
+
+    
+    
+
+  </script>
+
+    var gopher = 1;
+    if (gopher < 10) 
+   console.log(gopher);
+   } else {
+   console.log('potato');
+ }
