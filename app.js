dc0eb53acd0339bc23644ffe60deda0649f5770e
Jennifer Elton <justjenny40@hotmail.com>
2017-08-02 21:50:09 -0700

    ask first question

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 alert('Hello');
+var years = prompt('How many years have you been coding?');{
+  console.log( 'They have been coding: ' + years );
+  prompt( 'You have been coding for ' + years + '?');
+
+  var current = prompt( 'what are you currently learning?');
+  alert("Lets hope I get as good at " + current + 'as you are! ');
+  console.log( 'They are currently learning ' + current);
+
+}
+
+alert('Hello');
 var answer = 'England';
 for( var i = 0; i < 4; i ++ ) {
   var born = prompt( 'What country was I born in?' );
   console.log('What country was I born?');
-  if (born === 'England') {
+  if (born === 'england')str.toUpperCase;  {
     alert('You are correct!');
     break;
   }
