88e3b75b042c91ab3b945cc1cceffd5428043884
Jennifer Elton <justjenny40@hotmail.com>
2017-08-02 17:37:38 -0700

    ask first question

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
- /* <script>
-  alert('Hello');
-     var years=prompt('How many years have you been coding?');
-     console.log( 'They have been coding: ' +  years);
-
-    prompt( "You have been coding for " + years + "?");
-    var current= prompt( 'what are you currently learning?');
-
-    alert("Lets hope I get as good at " + current + " as you are! ");
-    console.log( 'They are currently learning ' + current);
-</script> */
-
-
-
-
-
-
-  <script>
-     alert prompt("Welcome to my page, lets see what you know about me!");
-     
-     
-
-    
-    
-
-  </script>
-
-    var gopher = 1;
-    if (gopher < 10) 
-   console.log(gopher);
-   } else {
-   console.log('potato');
- }
+alert('Hello');
+var answer = 'England';
+for( var i = 0; i < 4; i ++ ) {
+  var born = prompt( 'What country was I born in?' );
+  console.log('What country was I born?');
+  if (born === 'England') {
+    alert('You are correct!');
+    break;
+  }
+}
