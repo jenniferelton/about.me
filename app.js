6e2ff71c3d979ed7b3822dabc64cb39baaac7d9f
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-03 15:49:52 -0700

    added askCoding question

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 alert('Hello');
-var years = prompt('How many years have you been coding?');{
+
+function askCoding() {
+  var years = prompt('How many years have you been coding?');
   console.log( 'They have been coding: ' + years );
   prompt( 'You have been coding for ' + years + '?');
+}
+askCoding();
 
   var current = prompt( 'what are you currently learning?');
   alert("Lets hope I get as good at " + current + 'as you are! ');
   console.log( 'They are currently learning ' + current);
 
-}
+
+
 
 alert('Hello');
 var answer = 'England';
